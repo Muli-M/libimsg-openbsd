8b4946fc51f4f6fe0f5f8756c40cb12de1720e5c
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2020-09-02 12:30:46 +1000

    remake freezero() into a macro

--- compat.h
+++ compat.h
@@ -1,6 +1,15 @@
 /*
  * Copyright (c) 2020, Muli Management P/L, Wahroonga, NSW, Australia
  */
-int getdtablecount(void);
-void freezero(void *ptr, size_t size);
+
+#ifdef __linux__
+#define freezero(ptr, size)			\
+  {						\
+    if (ptr != NULL) {				\
+      memset(ptr, 0, size);			\
+      free(ptr);				\
+    } \
+  }
+#endif
+
 void *recallocarray(void *ptr, size_t oldnmemb, size_t newnmemb, size_t size);
